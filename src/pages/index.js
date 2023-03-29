0666c4667cef10b81efe3cbe5e74ce7f9f348431
Yinka <user.asyinka@gmail.com>
2023-03-29 18:43:10 +0100

    created home-page content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,11 @@
 import Navigation from "../sections/Navigation";
 import Hero from "../sections/Hero";
 import Footer from "../sections/Footer";
+import Callout from "../sections/Callout";
+
+export default function AboutUs(container){
+    Navigation(container);
+    Hero(container);
+    Callout(container);
+    Footer(container);
+}
