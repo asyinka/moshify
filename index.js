3d6433f834b0b0a78b82b1273e1bea58151830a3
Yinka <user.asyinka@gmail.com>
2023-03-29 20:19:19 +0100

    add renderer function

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
-import Navigation from "./src/sections/Navigation";
-import Hero from "./src/sections/Hero";
-import Pricing from "./src/sections/Pricing";
-import Plans from "./src/sections/plans";
-import Hosting from "./src/sections/Hosting";
-import Features from "./src/sections/Features";
-import Reviews from "./src/sections/Reviews";
-import Callout from "./src/sections/Callout";
-import Footer from "./src/sections/Footer";
+// import Navigation from "./src/sections/Navigation";
+// import Hero from "./src/sections/Hero";
+// import Pricing from "./src/sections/Pricing";
+// import Plans from "./src/sections/plans";
+// import Hosting from "./src/sections/Hosting";
+// import Features from "./src/sections/Features";
+// import Reviews from "./src/sections/Reviews";
+// import Callout from "./src/sections/Callout";
+// import Footer from "./src/sections/Footer";
 
-const appContainer = document.querySelector('#myApp');
+// const appContainer = document.querySelector('#myApp');
 
-Navigation(appContainer);
-Hero(appContainer);
-Pricing(appContainer);
-Plans(appContainer);
-Hosting(appContainer);
-Features(appContainer);
-Reviews(appContainer);
-Callout(appContainer);
-Footer(appContainer);
+// Navigation(appContainer);
+// Hero(appContainer);
+// Pricing(appContainer);
+// Plans(appContainer);
+// Hosting(appContainer);
+// Features(appContainer);
+// Reviews(appContainer);
+// Callout(appContainer);
+// Footer(appContainer);
+
+import Homepage from "./src/pages";
+
+
+function renderer (pageCall){
+// this function accept another function call to append an html content to the html
+    const appContainer = document.querySelector('#myApp');
+    appContainer.innerHTML = "";
+
+    if (pageCall != null){
+        pageCall(appContainer);
+    } else{
+        appContainer.innerHTML = `<div style="display: flex; justify-content: center; align-items: center; height: 100vh;">Something bad happened</div>`
+    }
+}
