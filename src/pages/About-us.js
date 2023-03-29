caf380fed49f6c0a4bafa09b843949b0b8447656
Yinka <user.asyinka@gmail.com>
2023-03-29 20:54:50 +0100

    create About Us page

---
+++ src/pages/About-us.js
@@ -0,0 +1,14 @@
+import Navigation from "../sections/Navigation";
+import Features from "../sections/Features";
+import Reviews from "../sections/Reviews";
+import Callout from "../sections/Callout";
+import Footer from "../sections/Footer";
+
+
+export default function AboutUs(container){
+    Navigation(container);
+    Features(container);
+    Reviews(container);
+    Callout(container);
+    Footer(container);
+}
